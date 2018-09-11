dc5600647002e5f2a8aa37fb501b4d61c082a3a7
lauraBo <BodellLaura@gmail.com>
2018-09-11 17:31:17 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,28 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
 
+
+//Adding an event listener to a DOM node doesn't require much logic — we just call addEventListener() on the node. addEventListener() takes two arguments: the name of the event,
+//and a function to handle the event.
+
+function init() {
+  let index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.which || e.detail);
+
+    if (code[index] === key) {
+      index++;
+
+      if (index === code.length) {
+        alert('Hurray!');
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
+
+init()
